905964f7085c8e73fc377285a9588f7512eb9472
Julianna Langston <julie.langston@gmail.com>
2024-05-08 17:11:02 -0400

    Export minimal type declaration

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from "@rollup/plugin-typescript";
+import copy from "rollup-plugin-copy";
 
 export default [
     {
@@ -22,6 +23,13 @@ export default [
                 format: "es"
             }
         ],
-        plugins: [typescript({tsconfig: "./tsconfig.json"})]
-    }
+        plugins: [
+            typescript({tsconfig: "./tsconfig.json"}),
+            copy({
+                targets: [
+                    {src: "src/plugin.d.ts", dest: "dist"}
+                ]
+            })
+        ]
+    },
 ]
